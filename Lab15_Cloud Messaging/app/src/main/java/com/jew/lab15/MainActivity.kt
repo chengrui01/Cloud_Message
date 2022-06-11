ae3e23a1940804437ee9e906ea1d4673b97b7104
PENG成瑞 <wsx891022@gmail.com>
2022-06-11 12:42:54 +0800

    Cloud

---
+++ Lab15_Cloud Messaging/app/src/main/java/com/jew/lab15/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.jew.lab15
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.iid.FirebaseInstanceId
+import com.google.firebase.messaging.FirebaseMessaging
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
